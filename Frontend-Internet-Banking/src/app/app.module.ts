d6bc068544f806cf56f59789b49b9924c642e942
galvs97@outlook.pt <galvs97@outlook.pt>
2020-02-08 16:01:20 -0300

    Feature tela login

--- Frontend-Internet-Banking/src/app/app.module.ts
+++ Frontend-Internet-Banking/src/app/app.module.ts
@@ -1,12 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponentComponent } from './login-component/login-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponentComponent,
   ],
   imports: [
     BrowserModule,
