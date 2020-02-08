15ecadadf990d5d5893295565859441d086bcf69
Lopes Boa <lopesboa1@gmail.com>
2020-02-08 19:07:01 -0300

    Revert "Dev"

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,40 +1,35 @@
-import 'dotenv/config'
-
 import * as cors from 'cors'
 import * as express from 'express'
-import { connect } from 'mongoose'
+import * as mongoose from 'mongoose'
 
-import MONGO_URI from './config/db.config'
-import routes from './routes/index.routes'
 
-// const MONGO_URI: string = process.env.APP_BD_URI as string
 class App {
   public app: express.Application
+  public PORT: number
   public constructor () {
     this.app = express()
-    process.env.TZ = 'America/Sao_Paulo'
-
+    this.PORT = 3333;
     this.middlewares()
     this.database()
     this.routes()
   }
 
   private middlewares (): void {
-    this.app.use(express())
     this.app.use(express.json())
     this.app.use(cors())
   }
 
   public database (): void {
-    connect(MONGO_URI, {
-      // useMongoClient:true,
+    mongoose.connect('URI', {
       useNewUrlParser: true,
       useUnifiedTopology: true
     })
   }
 
   private routes (): void {
-    this.app.use(routes)
+    this.app.get('/', (req, res) => {
+      return res.json({message: "Hellooo MB Team"})
+    })
   }
 }
 
