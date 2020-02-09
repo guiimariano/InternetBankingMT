d0983033a1a67c8353cbc2a1f9c358dfaf311d5d
Lopes Boa <lopesboa1@gmail.com>
2020-02-09 01:28:19 -0300

    feat: creating & implemente authMiddleware

--- backend/src/middlewares/auth.middleware.ts
+++ backend/src/middlewares/auth.middleware.ts
@@ -2,10 +2,11 @@ import { NextFunction, Request, Response } from 'express'
 import * as passport from 'passport'
 
 export default (req: Request, res :Response, next: NextFunction) => {
-  passport.authenticate('jwt', function(err, user, info) {
+  passport.authenticate('jwt', function(err, user, _info) {
     if(err) return next(err)
 
-    if(!user) return res.status(401).json({ message: 'Access denied - No token provided!'})
+    if(!user) return res.status(401)
+      .json({ message: 'Access denied - No token provided!' })
     
     req.user = user
     next()
