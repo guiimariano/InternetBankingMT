3016da4f2f0283e127a9a27c817cda8f25cd65cb
André Monteiro <dan.andre91@gmail.com>
2020-02-12 21:48:17 -0300

    [André Monteiro] - Tela de extrando filtrando periodos

--- Frontend-Internet-Banking/src/app/app.module.ts
+++ Frontend-Internet-Banking/src/app/app.module.ts
@@ -1,7 +1,8 @@
+import { registerLocaleData } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
-import LOCALE_ID from '@angular/common/locales/pt';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import localePt from '@angular/common/locales/pt';
+import { LOCALE_ID, NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -9,6 +10,8 @@ import { AppComponent } from './app.component';
 import { ExtratoComponent } from './extrato/extrato.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+
+registerLocaleData(localePt, 'pt');
 
 @NgModule({
   declarations: [
@@ -21,7 +24,8 @@ import { LoginComponent } from './login/login.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [{
     provide: LOCALE_ID,
--- Frontend-Internet-Banking/src/environments/environment.ts
+++ Frontend-Internet-Banking/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  API_URL: 'https://my-json-server.typicode.com/vitorfgsantos/fake-api'
+  API_URL: 'https://my-json-server.typicode.com/AndreAlbuquerque9/fake-api'
 };
 
 /*
