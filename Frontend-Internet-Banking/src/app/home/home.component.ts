5d9c5fecec99b009a6e0b4d227602f62776d95e5
galvs97@outlook.pt <galvs97@outlook.pt>
2020-02-13 14:02:46 -0300

    Retirada de conflitos, ajuste spinner no login

--- Frontend-Internet-Banking/src/app/home/home.component.ts
+++ Frontend-Internet-Banking/src/app/home/home.component.ts
@@ -7,16 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  
 
-<<<<<<< HEAD
   mobile = false;
   constructor() { }
-=======
-  constructor(
-  ) { }
->>>>>>> 846008d99eb638092a2cd423f6fa0c85e1d2d78d
-
   ngOnInit() {
     if (window.screen.width <= 425) {
       this.mobile = true;
--- Frontend-Internet-Banking/src/app/login/login.component.ts
+++ Frontend-Internet-Banking/src/app/login/login.component.ts
@@ -14,8 +14,6 @@ export class LoginComponent {
   senha: string;
   estaCarregando = false;
 
-  estaCarregando = false;
-
   constructor(
     private loginService: LoginService,
     private router: Router,
@@ -25,11 +23,8 @@ export class LoginComponent {
     this.loginService.logar(this.cpf, this.senha)
     .subscribe(response => {
       this.router.navigate(['home']);
-<<<<<<< HEAD
       this.estaCarregando = true;
-=======
       this.estaCarregando = false;
->>>>>>> 846008d99eb638092a2cd423f6fa0c85e1d2d78d
     }, error => {
       this.estaCarregando = true;
       console.error('NÃO DEU CERTO!! NÃO LOGOU!');
